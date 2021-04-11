d59b5eed7197ac66e7541a01934d163dcbe1bd8d
xwt981211 <xu_wanting2017@163.com>
2021-04-11 12:58:43 +0800

    model.pht
    is the optimal model

--- reports/M2/predict.py
+++ reports/M2/predict.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     with open('classes.txt') as f:
         label = list(map(lambda x:x.strip(), f.readlines()))
 
-    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    DEVICE = torch.device("cpu")
     print(DEVICE)
 
     model = torch.load('model.pht').to(DEVICE)
