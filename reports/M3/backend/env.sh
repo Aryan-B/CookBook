af3c6933c1250065c3ee9949cb7be024d8793ac2
xwt981211 <xu_wanting2017@163.com>
2021-05-07 02:21:53 +0800

    Update env.sh

--- reports/M3/backend/env.sh
+++ reports/M3/backend/env.sh
@@ -3,4 +3,5 @@ pip install flask==1.1.1
 pip install flask_cors
 pip install werkzeug==1.0.0
 pip install opencv-python
+pip install numpy==1.18.1
 conda install pytorch==1.5.0 torchvision==0.6.0 cpuonly -c pytorch
